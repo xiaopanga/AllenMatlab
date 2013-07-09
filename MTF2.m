0af7cea7441ee873f45000ec547bb51b48b10a60
allen wu <allenxiaopang@gmail.com>
2013-07-09 12:55:05 -0400

    updated MTF2`
    Converted MTF2 to a function

--- MTF2.m
+++ MTF2.m
@@ -1,25 +1,37 @@
-[FileName,PathName,~]=uigetfile({'*.TIF'});
+function [LPMM,mtf,rect,rect2,Group,Element]=MTF2(im,rect,rect2,Group,Element)
 
-FileLocation = strcat(PathName,FileName);
-im = imread(FileLocation);
-im = cast(im,'uint16');
-colormap(jet);
-imagesc(im);
-[X,Y,Mask,rect] = imcrop();
-imagesc(Mask);
-colormap(jet);
-[X2,Y2,Mask2,rect2] = imcrop();
-prompt = {'Group Number?','Element Number?'};
-dlg_title = 'Input';
-num_lines = 1;
-GroupAndElement = inputdlg(prompt,dlg_title,num_lines);
 
+im = cast(im,'double');
+if(nargin==1)
+    colormap(jet);
+    imagesc(im);
+    [Mask,rect] = imcrop();
+    imagesc(Mask);
+    colormap(jet)
+    [Mask2,rect2] = imcrop();
+    prompt = {'Group Number?','Element Number?'};
+    dlg_title = 'Input';
+    num_lines = 1;
+    GroupAndElement = inputdlg(prompt,dlg_title,num_lines);
+    Group = str2double(GroupAndElement{1});
+    Element = str2double(GroupAndElement{2});
+else if (nargin==5)
+        
+
+        [Mask,rect] = imcrop(im,rect);
+
+        [Mask2,rect2] = imcrop(Mask,rect2);
+    else
+        display('There should be either 5 or 1 inputs');
+        return;
+        
+    end
+end
 
 MaxIntensity = max(max(Mask2));
 MinIntensity = min(min(Mask2));
 
-Group = str2double(GroupAndElement{1})
-Element = str2double(GroupAndElement{2})
-LPMM = USAFTargetResolution(Group,Element)
-mtf = MTF(cast(MaxIntensity,'double'),cast(MinIntensity,'double'))
+
+LPMM = USAFTargetResolution(Group,Element);
+mtf = MTF(cast(MaxIntensity,'double'),cast(MinIntensity,'double'));
 
